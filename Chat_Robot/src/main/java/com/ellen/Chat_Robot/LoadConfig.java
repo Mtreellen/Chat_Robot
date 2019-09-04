0aa197088a9855580e7481b1b480786c4426e0f8
Mtreellen <mtreellen@163.com>
2019-09-04 22:37:24 +0800

    智能语音聊天机器人

---
+++ Chat_Robot/src/main/java/com/ellen/Chat_Robot/LoadConfig.java
@@ -0,0 +1,37 @@
+package com.ellen.Chat_Robot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.util.Properties;
+
+/**
+ * 加载配置文件，对语音合成的文本进行判断
+ * 1、若配置文件中key存在这个文本，则这个文本是命令，执行这条命令
+ * 2、若不是命令，则将文本传给图灵机器人
+ */
+public class LoadConfig {
+    Logger logger=LoggerFactory.getLogger(LoadConfig.class);
+
+    public String isOrder(String text,Properties properties){
+        String values = null;
+        logger.info("进入到加载配置文件中");
+        String fileName="D:\\IDEA代码\\Maven项目\\sophie_Intelligent_chat\\src\\main\\resources\\Config.properties";
+
+        try {
+            InputStream is=new FileInputStream(new File(fileName));
+            properties.load(is);
+            values=properties.getProperty(text);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return values;
+    }
+
+//    public static void main(String[] args) {
+//        LoadConfig loadConfig=new LoadConfig();
+//        String values=loadConfig.isOrder("打开计算器");
+//        System.out.println(values);
+//    }
+}
---
+++ Chat_Robot/src/main/java/com/ellen/Chat_Robot/Robot.java
@@ -0,0 +1,8 @@
+package com.ellen.Chat_Robot;
+
+public class Robot {
+    public static void main(String[] args) {
+        Sophie sophie=new Sophie();
+        sophie.start();
+    }
+}
